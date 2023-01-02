98fbafdf638cf8998d0259bbec057207f1cefcb6
hossam hakim <38508098+MaTb3aa@users.noreply.github.com>
2023-01-02 14:00:01 +0200

    Time: 87 ms (57.45%), Space: 41.9 MB (87.77%) - LeetHub

--- 0520-detect-capital/0520-detect-capital.js
+++ 0520-detect-capital/0520-detect-capital.js
@@ -3,9 +3,9 @@
  * @return {boolean}
  */
 var detectCapitalUse = function(word) {
-    var cap=0;
-    for(c in word){
-        if (word[c]>='A' && word[c]<='Z')cap+=1;
-    }
-    return (cap==0 || cap == word.length || (cap==1 && word[0]>='A' && word[0]<='Z'))
+    return (
+        word === word.toUpperCase() ||
+        word === word.toLowerCase() ||
+        (word[0] === word[0].toUpperCase() && word.slice(1) === word.slice(1).toLowerCase())
+    )
 };
