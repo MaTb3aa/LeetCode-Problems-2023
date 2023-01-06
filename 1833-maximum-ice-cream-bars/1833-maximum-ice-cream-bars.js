a722e1f1b9d22f3b3228e8b24a30c425ef1672fe
hossam hakim <38508098+MaTb3aa@users.noreply.github.com>
2023-01-06 16:37:06 +0200

    Time: 148 ms (100.00%), Space: 55.6 MB (91.67%) - LeetHub

--- 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.js
+++ 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.js
@@ -4,13 +4,17 @@
  * @return {number}
  */
 var maxIceCream = function(costs, coins) {
-    costs.sort(function (a,b){ return a - b});
-    var cnt = 0;
-    for (i in costs){
-        if(costs[i]<=coins){
-            cnt++;
-            coins-=costs[i];
-        }
+    let n = costs.length;
+    const mx = Math.max(...costs);
+    let cnt = 0 ;
+    let freq = Array(mx+1).fill(0);
+    
+    for(let x of costs)freq[x]+=1;
+    
+    for(let i = 1; i<= mx  ;i++){
+        let tmp = Math.min(Math.floor(coins/i),freq[i]);
+        cnt+=tmp;
+        coins-=tmp*i;
     }
     return cnt;
 };
