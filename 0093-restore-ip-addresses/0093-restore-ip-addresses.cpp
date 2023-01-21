8a812e184eae6ce1bff43427b38e7b3094f8ef09
hossam hakim <38508098+MaTb3aa@users.noreply.github.com>
2023-01-21 16:09:32 +0200

    Time: 150 ms (5.14%), Space: 31.8 MB (5.01%) - LeetHub

---
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    vector<string>ans;
+    string s="";
+    int n;
+    bool valid(string ip){
+        int cnt = 0,cur=0;
+        string tmp = "";
+        for(auto t : ip){
+            if(cur>255 )return 0;
+            if(t=='.'){
+                cnt++;
+                if(to_string(cur)!=tmp)return 0;
+                cur=0; tmp ="";
+            }
+            else {
+                tmp+=t;
+                cur= cur * 10+(t-'0');
+            }
+        }
+        if(to_string(cur)!=tmp || cur > 255)return 0;
+        return cnt==3 && ip.back()!='.';
+    }
+    void solve(int i,string ip,int len,string &s){
+          
+        
+        if(i==n){
+            if(valid(ip))ans.push_back(ip);
+            return ;
+        }
+        
+        if(len<3){
+            ip.push_back(s[i]);
+            solve(i+1,ip,len+1,s);
+            ip.pop_back();
+            
+            ip.push_back(s[i]);
+            ip.push_back('.');
+            solve(i+1,ip,0,s);
+            ip.pop_back();
+            ip.pop_back();
+        }
+       
+    }
+    vector<string> restoreIpAddresses(string s) {
+        n = s.size();
+        solve(0,"",0,s);
+        return ans;
+    }
+};
