4ff661f8580d2649e9ca3186d19e44b50f66da91
hossam hakim <38508098+MaTb3aa@users.noreply.github.com>
2023-01-23 16:04:59 +0200

    Time: 241 ms (43.43%), Space: 73.7 MB (10.24%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.cpp
+++ 0997-find-the-town-judge/0997-find-the-town-judge.cpp
@@ -1,23 +1,13 @@
 class Solution {
 public:
     
-    int find(int u,vector<int>&par){
-        if(par[u]==u)return u;
-        return par[u] = find(par[u],par);
-    }
-    void join(int a,int b,vector<int>&par){
-        a = find(a,par);
-        b = find(b,par);
-        if(a!=b){
-            par[a]=b;
-        }
-    }
+  
     int findJudge(int n, vector<vector<int>>& trust) {
         vector<int>par(n+1);
         for(auto t : trust)par[t[1]]+=t[0];
-        int ans = -1;
+        int ans = -1 , sum =n*(n+1) /2;
         for(int i=1;i<=n;i++){
-            if(par[i]+i == n*(n+1) /2 )ans= i;
+            if(par[i]+i == sum)ans= i;
         }
          for(auto t : trust)if(t[0]==ans)return -1;
         return ans;
