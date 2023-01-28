da1c7a26116a37d016fdeb40f011d1ee13df4b66
hossam hakim <38508098+MaTb3aa@users.noreply.github.com>
2023-01-28 21:44:20 +0200

    Time: 80 ms (53.99%), Space: 40.1 MB (5.32%) - LeetHub

--- 0352-data-stream-as-disjoint-intervals/0352-data-stream-as-disjoint-intervals.cpp
+++ 0352-data-stream-as-disjoint-intervals/0352-data-stream-as-disjoint-intervals.cpp
@@ -4,54 +4,38 @@ public:
     int vis[100003];
     SummaryRanges() {
         memset(vis,0,sizeof vis);
-        st.clear();
     }
     
     void addNum(int value) {
         value++;
-
         if(vis[value])return ;
+        vis[value]=1;
         if(vis[value-1] && vis[value+1]){
-           // cout<<value-1-1<<" "<<value-1<<" "<<value+1-1<<endl;
             auto it1 = st.upper_bound({value,0});
             if(it1!=st.begin())--it1;
-            
             auto it2 = st.lower_bound({value,0});
-           if(it2==st.end())it2--;
-            
-           // cout<<(*it1)[0]<<" "<<(*it1)[1]<<" && "<<(*it2)[0]<<" "<<(*it2)[0]<<endl;
-            vector<int>tmp = {(*it1)[0],(*it2)[1]};
+            if(it2==st.end())it2--;
+            st.insert({(*it1)[0],(*it2)[1]});
             st.erase(it1); st.erase(it2);
-            st.insert(tmp);
 
         }
         else if(vis[value+1]){
             auto it = st.upper_bound({value,0});
             if(it==st.end())--it;
-            //cout<<value-1<<endl;
-            //cout<<(*it)[0]<<" "<<(*it)[1]<<endl;
-
-             vector<int>tmp = {value,(*it)[1]};
+             st.insert({value,(*it)[1]});
              st.erase(it);
-             st.insert(tmp);
             
         }
         else if(vis[value-1]){
             vector<int>tmp;
             auto it = st.upper_bound({value,0});
             if(it!=st.begin())--it;
-
-             tmp = {(*it)[0],value};
+             st.insert({(*it)[0],value});
              st.erase(it);
-             st.insert(tmp);
+
             
         }
-        else {
-           vector<int>tmp = {value,value};
-            st.insert(tmp);
-        }
-        vis[value]=1;
-       
+        else st.insert({value,value});
     }
     
     vector<vector<int>> getIntervals() {
