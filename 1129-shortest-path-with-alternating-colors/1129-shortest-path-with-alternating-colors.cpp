62648e99314276004f6935f35ffb0cd69cd91f71
hossam hakim <38508098+MaTb3aa@users.noreply.github.com>
2023-02-11 16:28:41 +0200

    Time: 24 ms (58.44%), Space: 15.6 MB (40.66%) - LeetHub

--- 1129-shortest-path-with-alternating-colors/1129-shortest-path-with-alternating-colors.cpp
+++ 1129-shortest-path-with-alternating-colors/1129-shortest-path-with-alternating-colors.cpp
@@ -1,40 +1,39 @@
 class Solution {
 public:
-    enum {red,blue};
-    vector<vector<pair<int,int>>>g;
-    int BFS(int &n,int node,bool color){
-        queue<pair<int,bool>>q;
-        q.push({node,color});
-        vector<vector<int>>dist(n,vector<int>(2,0x3f3f3f3f));
-        dist[node][0]=0;
-        dist[node][1]=0;
+   
+      
+    vector<int> shortestAlternatingPaths(int n, vector<vector<int>>& redEdges, vector<vector<int>>& blueEdges) {
+        vector<vector<pair<int,bool>>>g(n);
+        
+        for(auto t : redEdges)
+           g[t[0]].push_back({t[1],0}); 
+        for(auto t : blueEdges)
+           g[t[0]].push_back({t[1],1}); 
+        
+        
+        //BFS
+        vector<vector<int>>vis(n,vector<int>(2));
+        vis[0][0] = vis[0][1] = true;
+        vector<int>dist(n,-1);
+        dist[0]=0;
+        // node dist color 
+        queue<tuple<int,int,int>>q;
+        q.push({0,0,-1});
+        
         while(q.size()){
-            auto cur = q.front();q.pop();
-            if(!cur.first)return min(dist[cur.first][0],dist[cur.first][1]);
-            for(auto child : g[cur.first]){
-                if(cur.second != child.second && dist[child.first][child.second] > dist[cur.first][cur.second]+1){
-                    dist[child.first][child.second] = dist[cur.first][cur.second]+1;
-                    q.push(child);
+            auto [curNode,path,curColor] = q.front();
+            q.pop();
+            for(auto [childNode,childColor] : g[curNode]){
+                
+                if(curColor != childColor && !vis[childNode][childColor]){
+                    if(dist[childNode]==-1) dist[childNode] = path+1;
+                    q.push({childNode,path+1,childColor});
+                    vis[childNode][childColor]=1;
                 }
             }
         }
-        return 0x3f3f3f3f;
-    }
-    vector<int> shortestAlternatingPaths(int n, vector<vector<int>>& redEdges, vector<vector<int>>& blueEdges) {
+        return dist;
         
-        g = vector<vector<pair<int,int>>>(n);
-        for(auto t : redEdges)
-            g[t[1]].push_back({t[0],red});
-        
-        for(auto t : blueEdges)
-            g[t[1]].push_back({t[0],blue});
-        
-        vector<int>ans(n);
-        
-        for(int i=1;i<n;i++){
-            ans[i] = min(BFS(n,i,0),BFS(n,i,1));
-            if(ans[i]==0x3f3f3f3f)ans[i] = -1;
-        }
-        return ans;
+      
     }
 };
