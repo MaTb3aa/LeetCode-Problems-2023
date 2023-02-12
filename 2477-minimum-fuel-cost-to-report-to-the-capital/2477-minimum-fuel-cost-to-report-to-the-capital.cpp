9aaf0abdd51b930f02deeebf47b4751ccc85d279
hossam hakim <38508098+MaTb3aa@users.noreply.github.com>
2023-02-12 14:31:32 +0200

    Time: 589 ms (86.52%), Space: 191.5 MB (72.79%) - LeetHub

--- 2477-minimum-fuel-cost-to-report-to-the-capital/2477-minimum-fuel-cost-to-report-to-the-capital.cpp
+++ 2477-minimum-fuel-cost-to-report-to-the-capital/2477-minimum-fuel-cost-to-report-to-the-capital.cpp
@@ -1,28 +1,28 @@
 class Solution {
 public:
     vector<vector<int>>g;
-    long long n,ans=0;
-    long long solve(int node,int par,int &seats){
-        long long ret = 0;
-        
-        for(auto child : g[node])
-            if(child!=par)
-                ret += 1 + solve(child,node,seats);
-            
-        if(par!=-1 )ans+=(ret+seats)/seats;
-        
-        return ret;
+    long long foul;
+    int dfs(int node,int par,int &seats){
+        int representatives = 1 ;
+        for(int child : g[node]){
+            if(child != par)
+                representatives+=dfs(child,node,seats);
+        }
+        if(node){
+            foul+=(representatives+seats-1)/seats;
+        }
+        return representatives;
     }
     
     long long minimumFuelCost(vector<vector<int>>& roads, int seats) {
-        n = roads.size();
+        int n = roads.size();
         g = vector<vector<int>>(n+1);
         for(int i=0;i<n;i++){
             g[roads[i][0]].push_back(roads[i][1]);
             g[roads[i][1]].push_back(roads[i][0]);
         }
-        solve(0,-1,seats);
-        return  ans;
+        dfs(0,-1,seats);
+        return  foul;
       
        
     }
