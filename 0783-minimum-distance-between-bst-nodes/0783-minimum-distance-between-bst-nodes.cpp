cced2804883a27170cb2ca96a7008c21eb05f27f
hossam hakim <38508098+MaTb3aa@users.noreply.github.com>
2023-02-17 14:40:56 +0200

    Time: 0 ms (100.00%), Space: 9.8 MB (30.06%) - LeetHub

--- 0783-minimum-distance-between-bst-nodes/0783-minimum-distance-between-bst-nodes.cpp
+++ 0783-minimum-distance-between-bst-nodes/0783-minimum-distance-between-bst-nodes.cpp
@@ -14,13 +14,13 @@ public:
     vector<int>v;
     void solve(TreeNode* root){
         if(!root)return ;
-        v.push_back(root->val);
         solve(root->left);
+        v.push_back(root->val);
+
         solve(root->right);
     }
     int minDiffInBST(TreeNode* root) {
         solve(root);
-        sort(v.begin(),v.end());
         int ans = 1e9;
         for(int i=1;i<v.size();i++)ans=min(ans,v[i]-v[i-1]);
         return ans;
