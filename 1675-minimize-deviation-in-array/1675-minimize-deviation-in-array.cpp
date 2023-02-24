be80875046b3f53450d33666c2f7c7f1444b6c58
hossam hakim <38508098+MaTb3aa@users.noreply.github.com>
2023-02-24 07:57:35 +0200

    Time: 1271 ms (5.08%), Space: 244.5 MB (7.63%) - LeetHub

--- 1675-minimize-deviation-in-array/1675-minimize-deviation-in-array.cpp
+++ 1675-minimize-deviation-in-array/1675-minimize-deviation-in-array.cpp
@@ -3,30 +3,31 @@ public:
     int minimumDeviation(vector<int>& nums) {
         multiset<int>mst;
         for(auto t : nums)mst.insert(t);
-        
         int ans = 1e9;
-        for(int i = 0 ; i < 3e6 ; i++){
-            ans = min (ans,abs((*(--mst.end())) -(*mst.begin()) ));
-            int a = *mst.begin();
-            if(a&1){
-                 a*=2;
-                 mst.erase(mst.begin());
-                mst.insert(a);
+        while(true){
+            ans = min(ans,abs((*(--mst.end())) -(*mst.begin()) ));
+            int x = *mst.begin();
+            if(x&1){
+                 x*=2;
+                mst.erase(mst.begin());
+                mst.insert(x);
             }
             else break;
             
         }
-         for(int i = 0 ; i < 3e6 ; i++){
-            ans = min (ans,abs((*(--mst.end())) -(*mst.begin()) ));
-            int b = *(--mst.end());
-            if( !(b&1) ){
-                b/=2;
+        while(true){
+                        ans = min(ans,abs((*(--mst.end())) -(*mst.begin()) ));
+
+            int x = *(--mst.end());
+            if( !(x&1) ){
+                x/=2;
                 mst.erase(prev(mst.end()));
-                mst.insert(b);
+                mst.insert(x);
             }
             else break;
-            
         }
+                    ans = min(ans,abs((*(--mst.end())) -(*mst.begin()) ));
+
         return ans;
     }
 };
