d23b9275cea7c29f18cf039bc97edc64c77c58b0
hossam hakim <38508098+MaTb3aa@users.noreply.github.com>
2023-03-12 22:07:24 +0200

    Time: 32 ms (40.78%), Space: 13.3 MB (58.02%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -10,27 +10,37 @@
  */
 class Solution {
 public:
-   ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
-    if (!l1) return l2;
-    if (!l2) return l1;
-    if (l1->val < l2->val) {
-        l1->next = mergeTwoLists(l1->next, l2);
-        return l1;
-    } else {
-        l2->next = mergeTwoLists(l1, l2->next);
-        return l2;
-    }
-}
+  
 
 ListNode* mergeKLists(vector<ListNode*>& lists) {
-    int n = lists.size();
-    if (n == 0) return NULL;
-    if (n == 1) return lists[0];
-    int mid = n / 2;
-    vector<ListNode*> left(lists.begin(), lists.begin() + mid);
-    vector<ListNode*> right(lists.begin() + mid, lists.end());
-    ListNode* l1 = mergeKLists(left);
-    ListNode* l2 = mergeKLists(right);
-    return mergeTwoLists(l1, l2);
+     // Define a lambda function to use as the comparator for the min-heap
+    auto cmp = [](const ListNode* a, const ListNode* b) {
+        return a->val > b->val;
+    };
+    
+    // Create a min-heap of size k, where k is the number of linked lists
+    priority_queue<ListNode*, vector<ListNode*>, decltype(cmp)> pq(cmp);
+    for (ListNode* node : lists) {
+        if (node) pq.push(node);
+    }
+    
+    // Create a dummy head node and a tail pointer for the result list
+    ListNode* dummy = new ListNode(0);
+    ListNode* tail = dummy;
+    
+    // Extract the minimum element from the heap, add it to the result list,
+    // and insert the next element from the same linked list into the heap
+    while (!pq.empty()) {
+        ListNode* node = pq.top();
+        pq.pop();
+        tail->next = node;
+        tail = node;
+        if (node->next) pq.push(node->next);
+    }
+    
+    // Free the dummy head node and return the result list
+    ListNode* head = dummy->next;
+    delete dummy;
+    return head;
 }
 };
