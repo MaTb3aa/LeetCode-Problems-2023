b3b8da6721917de7208c2984a8334b746aa2c0fc
hossam hakim <38508098+MaTb3aa@users.noreply.github.com>
2023-04-03 13:03:35 +0200

    Time: 102 ms (11.29%), Space: 42 MB (11.29%) - LeetHub

--- 0881-boats-to-save-people/0881-boats-to-save-people.cpp
+++ 0881-boats-to-save-people/0881-boats-to-save-people.cpp
@@ -1,23 +1,11 @@
 class Solution {
 public:
     int numRescueBoats(vector<int>& people, int limit) {
-        int st = 1 , ed = 1e9, cur = -1 , md;
-        multiset<int>mst;
-        
-        for(auto t : people)mst.insert(t);
-        
-        int cnt = 0;
-        while(mst.size()){
-            int cur = *mst.begin();
-            mst.erase(mst.begin());
-            if(mst.size()){
-                auto it = mst.lower_bound(limit-cur);
-               if(it==mst.end() || *it + cur > limit)it--;
-                if(it!=mst.end() && cur + *it <=limit){
-                    mst.erase(it);
-                }
-            }
-            cnt++;
+       sort(people.begin(),people.end());
+        int i = 0 , j = people.size()-1,cnt=0;
+        while(i<=j){
+            if(people[i] + people[j] <= limit)i++;
+            j--;cnt++;
         }
         return cnt;
     }
