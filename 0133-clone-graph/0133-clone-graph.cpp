15d39e7bdb7b08a5c2b4318f48ace9d7fe1d055b
hossam hakim <38508098+MaTb3aa@users.noreply.github.com>
2023-04-09 02:44:14 +0200

    Time: 8 ms (32.17%), Space: 8.9 MB (34.76%) - LeetHub

---
+++ 0133-clone-graph/0133-clone-graph.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    unordered_map<Node* , Node*> mp; 
+    Node* cloneGraph(Node* node) {
+        if(node == NULL) 
+        {
+            return NULL;
+        }
+        
+ 
+        if(mp.find(node) == mp.end()) // if not present in map
+        {
+            mp[node] = new Node(node -> val, {}); // make a copy
+            
+            for(auto adj: node -> neighbors) // travel in adjcant
+            {
+                mp[node] -> neighbors.push_back(cloneGraph(adj)); //add copy
+            }
+        }
+        
+        return mp[node];
+        
+    }
+};
