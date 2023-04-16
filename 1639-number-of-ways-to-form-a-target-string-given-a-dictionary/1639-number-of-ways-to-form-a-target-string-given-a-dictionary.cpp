623bafe96954747a79edbcd411d8e051578dfb00
hossam hakim <38508098+MaTb3aa@users.noreply.github.com>
2023-04-16 16:34:30 +0200

    Time: 305 ms (56.47%), Space: 39.7 MB (57.76%) - LeetHub

---
+++ 1639-number-of-ways-to-form-a-target-string-given-a-dictionary/1639-number-of-ways-to-form-a-target-string-given-a-dictionary.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int n,m,sz;
+    int dp[1001][1001];
+    vector<vector<int>>freq;
+    const int mod = 1e9+7;
+    int solve(int i,int k,vector<string>& words, string& target){
+        if(i==n)return 1;
+        if(k==m)return 0;
+        int &ret = dp[i][k];
+        if(~ret)return ret;
+        ret = 0 ;
+        ret += solve(i,k+1,words,target)%mod;
+        int cnt = freq[k][target[i]-'a'];
+        if (cnt) 
+            ret+=(1LL*(solve(i+1,k+1,words,target)%mod)*cnt)%mod;
+        return ret%mod;
+    }
+    int numWays(vector<string>& words, string target) {
+        n = target.size();
+        m = words[0].size();
+        sz = words.size();
+        freq = vector<vector<int>>(m,vector<int>(26));
+
+        for(auto word : words){
+            for(int k = 0 ; k  < m ; k++)
+                freq[k][word[k]-'a']++;
+        }
+
+        memset(dp,-1,sizeof dp);
+        return solve(0,0,words,target);
+    }
+};
