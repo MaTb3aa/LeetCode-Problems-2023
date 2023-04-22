f04d55dc57683410af7e17883ec7adbcad76318f
hossam hakim <38508098+MaTb3aa@users.noreply.github.com>
2023-04-23 00:33:28 +0200

    Time: 43 ms (81.72%), Space: 7.4 MB (87.98%) - LeetHub

---
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int dp[505][505];
+    int solve(int i,int j,string &s){
+        if(i>=j)return 0;
+        if(i+1==j)return s[i] != s[j];
+        int &ret = dp[i][j];
+        if(~ret)return ret;
+        ret = 1e9;
+        if(s[i]==s[j])
+            ret=min(ret,solve(i+1,j-1,s));
+        ret=min(ret,solve(i+1,j,s)+1);
+        ret=min(ret,solve(i,j-1,s)+1);
+        return ret;
+    }
+    int minInsertions(string s) {
+        memset(dp,-1,sizeof dp);
+        return solve(0,s.size()-1,s);
+    }
+};
