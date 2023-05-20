3f3053b3beb82414e313f845541d573824024737
hossam hakim <38508098+MaTb3aa@users.noreply.github.com>
2023-05-20 21:56:52 +0300

    Time: 3 ms (57.42%), Space: 8.4 MB (50.69%) - LeetHub

--- 0399-evaluate-division/0399-evaluate-division.cpp
+++ 0399-evaluate-division/0399-evaluate-division.cpp
@@ -1,52 +1,45 @@
 class Solution {
 public:
+    
+    int n;
+    map<string,vector<pair<string,double>>>graph;
 
-typedef unordered_map<string, unordered_map<string, double>> Graph;
+    double BFS(string src,string des){
 
-void addEdge(Graph& graph, const string& u, const string& v, double weight) {
-    graph[u][v] = weight;
-    graph[v][u] = 1 / weight;
-}
-double dfs(const Graph& graph, const string& start, const string& end, unordered_map<string, bool>& visited) {
-   
-    if (graph.find(start) == graph.end() || graph.find(end) == graph.end() || visited[start]) {
-        return -1.0;
-    }
+        if(graph.find(src)==graph.end() )return -1;
 
-    if (start == end) {
-        return 1.0;
-    }
-
-    visited[start] = true;
-    for (const auto& neighbor : graph.at(start)) {
-        double result = dfs(graph, neighbor.first, end, visited);
-        if (result != -1.0) {
-            return neighbor.second * result;
+        set<string>vis;
+        queue<pair<string,double>>q;
+        q.push({src,1});
+        vis.insert(src);
+        while(q.size()){
+            string cur = q.front().first;
+            double cost = q.front().second;
+            if(cur==des)return cost;
+            q.pop();
+            for(auto t : graph[cur]){
+                if(vis.find(t.first)==vis.end()){
+                    vis.insert(t.first);
+                    q.push({t.first,(cost*t.second)});
+                }
+            }
         }
+        return -1;
     }
-
-    visited[start] = false;
-    return -1.0;
-}
-
-    vector<double> calcEquation(vector<vector<string>>& equations, vector<double>& values, vector<vector<string>>& queries) {
-        Graph graph;
-
-    for (int i = 0; i < equations.size(); i++) {
-        const string& a = equations[i][0];
-        const string& b = equations[i][1];
-        double val = values[i];
-        addEdge(graph, a, b, val);
-    }
-
-    vector<double> results;
-    for (const auto& query : queries) {
-        const string& start = query[0];
-        const string& end = query[1];
-        unordered_map<string, bool> visited;
-        results.push_back(dfs(graph, start, end, visited));
-    }
-
-    return results;
+    
+    
+    vector<double> calcEquation(vector<vector<string>>& equations, vector<double>& values, vector<vector<string>>& queries)     {
+       n = equations.size();
+       for(int i=0;i<n;i++){
+           graph[equations[i][0]].push_back({equations[i][1],values[i]});
+           graph[equations[i][1]].push_back({equations[i][0],1.0/values[i]});
+       }
+      
+        vector<double>ans;
+        for(auto t: queries){
+            ans.push_back(BFS(t[0],t[1]));
+        }
+        return ans;
+        
     }
 };
